6cae073e9352b5da98ce439a0f9fc8ad5c94d146
Gazi Göktuğ <115394107+goktugkurnaz26@users.noreply.github.com>
2023-08-02 13:33:28 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import BookCreate from "./components/BookCreate"
 const App = () => {
     const [books, setBooks] = useState([]) 
     const createBook = (title) => {
-      // KÖTÜ KODLAMA 
       books.push({id:123,title:title})
       setBooks(books)
     }
